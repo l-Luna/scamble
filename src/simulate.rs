7b701a2463f426363d439c2834c2c9b4ab67ecc3
l-Luna <izzeldeen03@gmail.com>
2025-03-15 22:31:48 +0000

    a

--- src/simulate.rs
+++ src/simulate.rs
@@ -1,9 +1,3 @@
-use crate::raw_bindings::{
-    FMOD_CHANNELGROUP, FMOD_SYSTEM, FMOD_System_Create, FMOD_System_CreateChannelGroup,
-    FMOD_System_PlayDSP, FMOD_VERSION,
-};
-use std::ffi::CString;
-use std::ptr;
 use std::thread::sleep;
 use std::time::Duration;
 use fmod::system::System;
@@ -16,7 +10,7 @@ fn simulate() {
     let cgroup = system.create_channel_group("group").fm_unwrap();
     let dsp = system.create_dsp_from_description(crate::DESC).fm_unwrap();
     let channel = system.play_dsp(&dsp, &cgroup, false).fm_unwrap();
-    for i in 0..7 {
+    for _ in 0..7 {
         sleep(Duration::from_secs(1))
     }
     system.release().unwrap()
