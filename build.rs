cfb3c580c3360726c7fe33c775eb1e98f2a1a9f1
l-Luna <izzeldeen03@gmail.com>
2025-03-15 19:09:03 +0000

    scamble

---
+++ build.rs
@@ -0,0 +1,31 @@
+use bindgen::EnumVariation;
+use std::{env, fs};
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rustc-link-search=./libs/");
+    println!("cargo:rustc-link-lib=fmod");
+
+    let bindings = bindgen::Builder::default()
+        .header("libs/wrapper.h")
+        .default_enum_style(EnumVariation::Rust {
+            non_exhaustive: true,
+        })
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let target = &out_path.join("bindings.rs");
+    bindings
+        .write_to_file(target)
+        .expect("Couldn't write bindings!");
+
+    // slightly hacky way to append the right link mode for windows
+    let mut st = fs::read_to_string(target).expect("Couldn't update bindings!");
+    st = st.replace(
+        "unsafe extern \"C\" {",
+        "#[cfg_attr(windows, link(name = \"fmod.dll\", kind = \"raw-dylib\"))]\nunsafe extern \"C\" {"
+    );
+    fs::write(target, st).expect("Couldn't update bindings!");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,86 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use crate::bindings::FMOD_RESULT::FMOD_OK;
+use crate::bindings::{
+    FMOD_BOOL, FMOD_CHANNELMASK, FMOD_DSP_BUFFER_ARRAY, FMOD_DSP_DESCRIPTION,
+    FMOD_DSP_PROCESS_OPERATION, FMOD_DSP_STATE, FMOD_PLUGIN_SDK_VERSION, FMOD_RESULT,
+    FMOD_SPEAKERMODE,
+};
+use rand::{Rng, rng};
+use std::ptr;
+use std::slice::from_raw_parts_mut;
+
+pub mod bindings;
+
+static mut DESC: FMOD_DSP_DESCRIPTION = FMOD_DSP_DESCRIPTION {
+    pluginsdkversion: FMOD_PLUGIN_SDK_VERSION,
+    name: [
+        b'S' as i8, b'c' as i8, 'a' as i8, 'm' as i8, 'b' as i8, 'l' as i8, 'e' as i8,
+        '\0' as i8, '\0' as i8, '\0' as i8, '\0' as i8, '\0' as i8, '\0' as i8, '\0' as i8,
+        '\0' as i8, '\0' as i8, '\0' as i8, '\0' as i8, '\0' as i8, '\0' as i8, '\0' as i8,
+        '\0' as i8, '\0' as i8, '\0' as i8, '\0' as i8, '\0' as i8, '\0' as i8, '\0' as i8,
+        '\0' as i8, '\0' as i8, '\0' as i8, '\0' as i8,
+    ],
+    version: 1,
+    numinputbuffers: 0,
+    numoutputbuffers: 1,
+    create: None,
+    release: None,
+    reset: None,
+    read: None,
+    process: Some(process),
+    setposition: None,
+    numparameters: 0,
+    paramdesc: ptr::null_mut(),
+    setparameterfloat: None,
+    setparameterint: None,
+    setparameterbool: None,
+    setparameterdata: None,
+    getparameterfloat: None,
+    getparameterint: None,
+    getparameterbool: None,
+    getparameterdata: None,
+    shouldiprocess: Some(should_i_process),
+    userdata: ptr::null_mut(),
+    sys_register: None,
+    sys_deregister: None,
+    sys_mix: None,
+};
+
+#[unsafe(no_mangle)]
+unsafe extern "stdcall" fn FMODGetDSPDescription() -> *mut FMOD_DSP_DESCRIPTION {
+    &raw mut DESC
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+extern "C" fn process(
+    dsp_state: *mut FMOD_DSP_STATE,
+    length: ::std::os::raw::c_uint,
+    inbufferarray: *const FMOD_DSP_BUFFER_ARRAY,
+    outbufferarray: *mut FMOD_DSP_BUFFER_ARRAY,
+    inputsidle: FMOD_BOOL,
+    op: FMOD_DSP_PROCESS_OPERATION,
+) -> FMOD_RESULT {
+    unsafe {
+        if op == FMOD_DSP_PROCESS_OPERATION::FMOD_DSP_PROCESS_QUERY {
+            *(*outbufferarray).buffernumchannels = 1;
+            FMOD_OK
+        } else {
+            let ulen = length as usize;
+            let chann = ((*(*outbufferarray).buffernumchannels) as usize);
+            let buf = from_raw_parts_mut(*(*outbufferarray).buffers, ulen * chann);
+            for i in 0..ulen * chann {
+                buf[i] = rng().random_range(0. ..1.);
+            }
+            FMOD_OK
+        }
     }
+}
+
+extern "C" fn should_i_process(
+    dsp_state: *mut FMOD_DSP_STATE,
+    inputsidle: FMOD_BOOL,
+    length: ::std::os::raw::c_uint,
+    inmask: FMOD_CHANNELMASK,
+    inchannels: ::std::os::raw::c_int,
+    speakermode: FMOD_SPEAKERMODE,
+) -> FMOD_RESULT {
+    FMOD_OK
 }
